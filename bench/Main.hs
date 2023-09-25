67e2abcd931e3c2505c4bd4ea3f3b0ed8a8f2b20
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-09-25 16:02:15 -0400

    benchmark tweak

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Control.DeepSeq (force)
+import Control.DeepSeq (deepseq, force)
 import Control.Exception (evaluate)
 import List.Shuffle qualified as List
 import System.Random qualified as Random
@@ -8,9 +8,13 @@ import Test.Tasty.Bench (bench, defaultMain, whnf)
 
 main :: IO ()
 main = do
-  let list = [1 .. 1000] :: [Int]
+  let list = [1 .. 1000000] :: [Int]
   _ <- evaluate (force list)
 
   defaultMain
-    [ bench "shuffle" (whnf (last . fst . List.shuffle list) (Random.mkStdGen 0))
+    [ bench "shuffle" (whnf shuffle (list, Random.mkStdGen 0))
     ]
+
+shuffle :: ([Int], Random.StdGen) -> ()
+shuffle (list, gen) =
+  deepseq (fst (List.shuffle list gen)) ()
